f2c0a069c6bcaf046cfc5596fff90ede8711bc67
Minho Kwon <minhok3@gmail.com>
2022-04-04 17:12:03 -0400

    data fetching

--- speculum-mundi-client/src/routes/SearchResult/SearchResult.tsx
+++ speculum-mundi-client/src/routes/SearchResult/SearchResult.tsx
@@ -1,17 +1,20 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import "./SearchResult.css";
 import NavHeader from "../../NavHeader/NavHeader";
 
-function getSearchList() {
-  fetch("../../Assets/mock-data.json").then((response) =>
-    console.log(response)
-  );
-}
-
 export default function SearchResult() {
   const { searchText } = useParams();
+  const [searchList, setSearchList] = useState([]);
+
+  function getSearchList() {
+    fetch("../src/Assets/mock-data.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => console.log(response));
+  }
 
   useEffect(() => {
     getSearchList();

b98fff00f1302aa734462da8f95d527f3961c0e8
Minho Kwon <minhok3@gmail.com>
2022-04-20 02:41:29 -0400

    custom node starting

---
+++ speculum-mundi-client/src/routes/TimelineView/TimelineNode/TimelineNodeModel.tsx
@@ -0,0 +1,6 @@
+import { NodeModel, DefaultPortModel } from "@projectstorm/react-diagrams";
+import { BaseModelOptions } from "@projectstorm/react-canvas-core";
+
+export interface TSCustomNodeModelOptions extends BaseModelOptions {
+  color?: string;
+}

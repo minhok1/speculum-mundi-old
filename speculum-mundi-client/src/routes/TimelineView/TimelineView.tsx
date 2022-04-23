cf768fe8d001e224255d5669805283ee5f46a165
Minho Kwon <minhok3@gmail.com>
2022-04-22 22:10:30 -0400

    tempsave

--- speculum-mundi-client/src/routes/TimelineView/TimelineView.tsx
+++ speculum-mundi-client/src/routes/TimelineView/TimelineView.tsx
@@ -4,9 +4,35 @@ import "./TimelineView.css";
 import { useState, useEffect } from "react";
 import { Abstract, TimelineEvent } from "../../types";
 import TimelineSearch from "./TimelineSearch";
+import { Options, Edge, Node } from "vis-network/standalone/esm/vis-network";
+import useVisNetwork from "./UseVisNetwork";
 
 export default function TimelineView() {
   const [abstracts, setAbstracts] = useState<Abstract[]>([]);
+  const [nodes, setNodes] = useState<Node[]>([]);
+  const [edges, setEdges] = useState<Edge[]>([]);
+
+  // const nodes: Node[] = [
+  //   {
+  //     id: 1,
+  //     label: "test",
+  //     title: "ПАО Т Плюс",
+  //     // level: 1,
+  //     group: "struct",
+  //   },
+  // ]
+
+  // const edges: Edge[] = [
+  //   { from: 1, to: 2, id: 1 },
+  //   { from: 1, to: 3, id: 6 },
+  // ];
+  const options: Options = {};
+
+  const { ref, network } = useVisNetwork({
+    options,
+    edges,
+    nodes,
+  });
 
   const configureAbstracts = () => {
     abstracts.forEach((abs, absIndex) => {
@@ -28,7 +54,7 @@ export default function TimelineView() {
     <div>
       <NavHeader />
       <TimelineSearch state={abstracts} stateChanger={setAbstracts} />
-      <div className="canvas-container"></div>
+      <div className="timeline-canvas" ref={ref} />
     </div>
   );
 }

0dd16b05f868f671153e383d7d6ed8a4890f5770
Minho Kwon <minhok3@gmail.com>
2022-06-11 17:30:17 -0400

    map included

--- speculum-mundi-client/src/routes/GeoView/GeoView.tsx
+++ speculum-mundi-client/src/routes/GeoView/GeoView.tsx
@@ -2,12 +2,29 @@ import "./GeoView.css";
 import NavSidebar from "../../NavSidebar/NavSidebar";
 import ProfileHeader from "../../ProfileHeader/ProfileHeader";
 
+import GoogleMapReact from "google-map-react";
+
 export default function GeoView() {
+  const apiKey = "AIzaSyD759inzfhgp_XQWSUQgsXRdpvOgb24gFk";
+
   return (
     <div className="page-container">
       <NavSidebar />
       <div className="contents-container">
         <ProfileHeader pageTitle="Map" />
+        <div className="dashboard-container map-contents">
+          <div className="map-left-container">hewwo</div>
+          <div className="map-right-container">
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: apiKey }}
+              defaultCenter={{
+                lat: 59.95,
+                lng: 30.33,
+              }}
+              defaultZoom={11}
+            ></GoogleMapReact>
+          </div>
+        </div>
       </div>
     </div>
   );

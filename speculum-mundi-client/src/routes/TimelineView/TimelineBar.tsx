d5fb83bcd82d956766c46e88315f4894b7492771
Minho Kwon <minhok3@gmail.com>
2022-10-30 23:04:51 -0400

    tempsave

--- speculum-mundi-client/src/routes/TimelineView/TimelineBar.tsx
+++ speculum-mundi-client/src/routes/TimelineView/TimelineBar.tsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
 import "./TimelineBar.css";
 
 export default function TimelineBar() {
-  return <div className="timeline-bar"></div>;
+  const [cursorPosition, setCursorPosition] = useState(-1);
+
+  const configureCursorPosition = (e: any) => {
+    setCursorPosition(e.nativeEvent.x);
+  };
+
+  return (
+    <div
+      onMouseMove={(e) => {
+        configureCursorPosition(e);
+      }}
+      onMouseLeave={() => {
+        setCursorPosition(-1);
+      }}
+      className="timeline-bar"
+    >
+      {cursorPosition === -1 ? (
+        <></>
+      ) : (
+        <div className="timeline-year">{cursorPosition}</div>
+      )}
+    </div>
+  );
 }

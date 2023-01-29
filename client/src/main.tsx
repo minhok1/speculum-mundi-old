bb9cf8861d3ae3c88b4be89df982aade05463a7e
Minho Kwon <minhok3@gmail.com>
2023-01-29 02:04:57 -0500

    bootstrap installed?

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import App from "./App";
 import "./index.css";
 
